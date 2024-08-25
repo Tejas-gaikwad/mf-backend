d7486c44b0e34ac90507c29090a2317be52abda3
Tejas Gaikwad <tejasgaikwad0504@gmail.com>
2024-08-25 22:59:14 +0530

    added api

--- src/controllers/transact_controller.js
+++ src/controllers/transact_controller.js
@@ -3,14 +3,12 @@ const InvestorSchema = require('../models/investor');
 
 
 const GetCurrentTransactionLog = async (req, res) => {
-    
     try{
         const investor_uid = req.investor.investor_uid;
         const investor = await InvestorSchema.findOne({investor_uid});
         if (!investor) {
           return res.status(404).json({ message: 'Investor not found' });
         }
-
             const clientId = req.params.clientId;
             const client = await clientSchema.findById(clientId);
             
@@ -22,7 +20,7 @@ const GetCurrentTransactionLog = async (req, res) => {
               "data" : [
                 {
                   "client_name" : (client.user_details.first_name ? client.user_details.first_name : "") + " " + (client.user_details.last_name ? client.user_details.last_name : ""),
-                  "transaction_date" : "20 August 2024",
+                  "transaction_execution_date" : "20 August 2024",
                   "folio_no" : "10569084",
                   "name" : "HDFC Flexicap Fund - Growth option",
                   "amount" : "1000",
@@ -33,7 +31,7 @@ const GetCurrentTransactionLog = async (req, res) => {
                 },
                 {
                     "client_name" : (client.user_details.first_name ? client.user_details.first_name : "") + " " + (client.user_details.last_name ? client.user_details.last_name : ""),
-                    "transaction_date" : "12 August 2024",
+                    "transaction_execution_date" : "12 August 2024",
                     "folio_no" : "91023008102",
                     "name" : "AXIS Liquid Fund -Regular Growth",
                     "amount" : "50000",
@@ -44,7 +42,7 @@ const GetCurrentTransactionLog = async (req, res) => {
                 },
                 {
                     "client_name" : (client.user_details.first_name ? client.user_details.first_name : "") + " " + (client.user_details.last_name ? client.user_details.last_name : ""),
-                    "transaction_date" : "5 July 2024",
+                    "transaction_execution_date" : "5 July 2024",
                     "folio_no" : "8202527",
                     "name" : "ICICI Pruduntial Multicap Fund - Growth",
                     "amount" : "100000",
@@ -55,9 +53,6 @@ const GetCurrentTransactionLog = async (req, res) => {
                 },
               ]
             })
-      
-
-
     } catch(err){
             return res.status(500).send({
                 message: 'Error, Something went wrong.',
@@ -67,14 +62,12 @@ const GetCurrentTransactionLog = async (req, res) => {
 }
 
 const GetFutureTransactionLog = async (req, res) => {
-    
     try{
         const investor_uid = req.investor.investor_uid;
         const investor = await InvestorSchema.findOne({investor_uid});
         if (!investor) {
           return res.status(404).json({ message: 'Investor not found' });
         }
-
             const clientId = req.params.clientId;
             const client = await clientSchema.findById(clientId);
             
@@ -86,7 +79,7 @@ const GetFutureTransactionLog = async (req, res) => {
               "data" : [
                 {
                   "client_name" : (client.user_details.first_name ? client.user_details.first_name : "") + " " + (client.user_details.last_name ? client.user_details.last_name : ""),
-                  "transaction_date" : "20 August 2024",
+                  "transaction_execution_date" : "20 September 2024",
                   "folio_no" : "10569084",
                   "name" : "HDFC Flexicap Fund - Growth option",
                   "amount" : "1000",
@@ -97,7 +90,7 @@ const GetFutureTransactionLog = async (req, res) => {
                 },
                 {
                     "client_name" : (client.user_details.first_name ? client.user_details.first_name : "") + " " + (client.user_details.last_name ? client.user_details.last_name : ""),
-                    "transaction_date" : "12 August 2024",
+                    "transaction_execution_date" : "12 September 2024",
                     "folio_no" : "91023008102",
                     "name" : "AXIS Liquid Fund -Regular Growth",
                     "amount" : "50000",
@@ -109,9 +102,6 @@ const GetFutureTransactionLog = async (req, res) => {
               
               ]
             })
-      
-
-
     } catch(err){
             return res.status(500).send({
                 message: 'Error, Something went wrong.',
@@ -120,6 +110,408 @@ const GetFutureTransactionLog = async (req, res) => {
     }
 }
 
+
+const UpdateTransactionExecutionDate = async (req, res) => {
+
+  try{
+    const investor_uid = req.investor.investor_uid;
+    const {date, clientId} = req.body;
+    const investor = await InvestorSchema.findOne({investor_uid});
+    if (!investor) {
+      return res.status(404).json({ message: 'Investor not found' });
+    }
+    const client = await clientSchema.findByIdAndUpdate(clientId);
+      
+    if (!client) {
+        return res.status(404).json({ "status" : false, message: 'Client not found' });
+    }
+
+    return res.status(201).json({
+      "status" : true,
+      "data" : [
+        {
+          "client_name" : (client.user_details.first_name ? client.user_details.first_name : "") + " " + (client.user_details.last_name ? client.user_details.last_name : ""),
+          "transaction_execution_date" : date,
+          "folio_no" : "10569084",
+          "name" : "HDFC Flexicap Fund - Growth option",
+          "amount" : "1000",
+          "BSE_client_id": "8787",
+          "member_id" : "11689",
+          "moh" : "SI",
+          "message" : "Invalid user name and password"
+        },
+      ]
+    });
+
+  } catch(err) {
+    return res.status(201).json({
+      "status" : false,
+      "error" : err.message
+    });
+  }
+
+}
+
+  const InvestInTopSchemes = async (req, res) => {
+    const investor_uid = req.investor.investor_uid;
+    const  clientId = req.params.clientId;
+    const investor = await InvestorSchema.findOne({investor_uid});
+    if (!investor) {
+      return res.status(404).json({ message: 'Investor not found' });
+    }
+    const client = await clientSchema.findOne(clientId);
+      
+    if (!client) {
+        return res.status(404).json({ "status" : false, message: 'Client not found' });
+    }
+
+    return res.status(201).json({
+      "status" : true,
+      "data" : [
+        {
+          "scheme_name" : "bharath sir best",
+          "scheme_id": "100",
+          "funds_list" : [
+            {
+              "fund_name" : "360 One Focused Equity Fund Regular Plan - Growth",
+              "latest_nav" : "21.63",
+              "like" : "0",
+              "fund_type" : "equity-index funds or ETFs",
+              "one_month_abs_return" : "5.31",
+              "one_year_XIRR_return" : "63.99",
+              "three_year_XIRR_return" : "21.75",
+              "five_year_XIRR_return" : "N/A",
+            },
+            {
+              "fund_name" : "Bandhan Small Cap Fund Regular Plan - IDCW payout",
+              "latest_nav" : "37.01",
+              "like" : "0",
+              "fund_type" : "equity-Small cap fund",
+              "one_month_abs_return" : "9.60",
+              "one_year_XIRR_return" : "61.77",
+              "three_year_XIRR_return" : "22.27",
+              "five_year_XIRR_return" : "N/A",
+            }
+          ]
+        },
+        {
+          "scheme_name" : "bhavesh sir Small cap",
+          "scheme_id": "101",
+          "funds_list" : [
+            {
+              "fund_name" : "3630 One Focused Equity Fund Regular Plan - Growth",
+              "latest_nav" : "21.63",
+              "like" : "0",
+              "fund_type" : "equity-index funds or ETFs",
+              "one_month_abs_return" : "5.31",
+              "one_year_XIRR_return" : "63.99",
+              "three_year_XIRR_return" : "21.75",
+              "five_year_XIRR_return" : "N/A",
+            }
+          ]
+        },
+        {
+          "scheme_name" : "Black Pearl Best Sip",
+          "scheme_id": "102",
+          "funds_list" : [
+            {
+              "fund_name" : "360 One Focused Equity Fund Regular Plan - Growth",
+              "latest_nav" : "21.63",
+              "like" : "0",
+              "fund_type" : "equity-index funds or ETFs",
+              "one_month_abs_return" : "5.31",
+              "one_year_XIRR_return" : "63.99",
+              "three_year_XIRR_return" : "21.75",
+              "five_year_XIRR_return" : "N/A",
+            },
+            {
+              "fund_name" : "Bandhan Small Cap Fund Regular Plan - IDCW payout",
+              "latest_nav" : "37.01",
+              "like" : "0",
+              "fund_type" : "equity-Small cap fund",
+              "one_month_abs_return" : "9.60",
+              "one_year_XIRR_return" : "61.77",
+              "three_year_XIRR_return" : "22.27",
+              "five_year_XIRR_return" : "N/A",
+            }
+          ]
+        },
+        {
+          "scheme_name" : "Conservative",
+          "scheme_id": "103",
+          "funds_list" : [
+            {
+              "fund_name" : "360 One Focused Equity Fund Regular Plan - Growth",
+              "latest_nav" : "21.63",
+              "like" : "0",
+              "fund_type" : "equity-index funds or ETFs",
+              "one_month_abs_return" : "5.31",
+              "one_year_XIRR_return" : "63.99",
+              "three_year_XIRR_return" : "21.75",
+              "five_year_XIRR_return" : "N/A",
+            },
+         
+          ]
+        },
+        {
+          "scheme_name" : "Gaurav's best scheme",
+          "scheme_id": "104",
+          "funds_list" : [
+            {
+              "fund_name" : "360 One Focused Equity Fund Regular Plan - Growth",
+              "latest_nav" : "21.63",
+              "like" : "0",
+              "fund_type" : "equity-index funds or ETFs",
+              "one_month_abs_return" : "5.31",
+              "one_year_XIRR_return" : "63.99",
+              "three_year_XIRR_return" : "21.75",
+              "five_year_XIRR_return" : "N/A",
+            },
+            {
+              "fund_name" : "Bandhan Small Cap Fund Regular Plan - IDCW payout",
+              "latest_nav" : "37.01",
+              "like" : "0",
+              "fund_type" : "equity-Small cap fund",
+              "one_month_abs_return" : "9.60",
+              "one_year_XIRR_return" : "61.77",
+              "three_year_XIRR_return" : "22.27",
+              "five_year_XIRR_return" : "N/A",
+            }
+          ]
+        },
+        {
+          "scheme_name" : "Growth investment",
+          "scheme_id": "105",
+          "funds_list" : [
+            {
+              "fund_name" : "360 One Focused Equity Fund Regular Plan - Growth",
+              "latest_nav" : "21.63",
+              "like" : "0",
+              "fund_type" : "equity-index funds or ETFs",
+              "one_month_abs_return" : "5.31",
+              "one_year_XIRR_return" : "63.99",
+              "three_year_XIRR_return" : "21.75",
+              "five_year_XIRR_return" : "N/A",
+            },
+            {
+              "fund_name" : "Bandhan Small Cap Fund Regular Plan - IDCW payout",
+              "latest_nav" : "37.01",
+              "like" : "0",
+              "fund_type" : "equity-Small cap fund",
+              "one_month_abs_return" : "9.60",
+              "one_year_XIRR_return" : "61.77",
+              "three_year_XIRR_return" : "22.27",
+              "five_year_XIRR_return" : "N/A",
+            }
+          ]
+        },
+        {
+          "scheme_name" : "Jharna Mid Cap Funds",
+          "scheme_id": "106",
+          "funds_list" : [
+            {
+              "fund_name" : "360 One Focused Equity Fund Regular Plan - Growth",
+              "latest_nav" : "21.63",
+              "like" : "0",
+              "fund_type" : "equity-index funds or ETFs",
+              "one_month_abs_return" : "5.31",
+              "one_year_XIRR_return" : "63.99",
+              "three_year_XIRR_return" : "21.75",
+              "five_year_XIRR_return" : "N/A",
+            },
+            {
+              "fund_name" : "Bandhan Small Cap Fund Regular Plan - IDCW payout",
+              "latest_nav" : "37.01",
+              "like" : "0",
+              "fund_type" : "equity-Small cap fund",
+              "one_month_abs_return" : "9.60",
+              "one_year_XIRR_return" : "61.77",
+              "three_year_XIRR_return" : "22.27",
+              "five_year_XIRR_return" : "N/A",
+            }
+          ]
+        },
+        {
+          "scheme_name" : "M2m",
+          "scheme_id": "107",
+          "funds_list" : [
+            {
+              "fund_name" : "360 One Focused Equity Fund Regular Plan - Growth",
+              "latest_nav" : "21.63",
+              "like" : "0",
+              "fund_type" : "equity-index funds or ETFs",
+              "one_month_abs_return" : "5.31",
+              "one_year_XIRR_return" : "63.99",
+              "three_year_XIRR_return" : "21.75",
+              "five_year_XIRR_return" : "N/A",
+            },
+            {
+              "fund_name" : "Bandhan Small Cap Fund Regular Plan - IDCW payout",
+              "latest_nav" : "37.01",
+              "like" : "0",
+              "fund_type" : "equity-Small cap fund",
+              "one_month_abs_return" : "9.60",
+              "one_year_XIRR_return" : "61.77",
+              "three_year_XIRR_return" : "22.27",
+              "five_year_XIRR_return" : "N/A",
+            }
+          ]
+        },
+        {
+          "scheme_name" : "Saad Best Small Cap Funds",
+          "scheme_id": "108",
+          "funds_list" : [
+            {
+              "fund_name" : "360 One Focused Equity Fund Regular Plan - Growth",
+              "latest_nav" : "21.63",
+              "like" : "0",
+              "fund_type" : "equity-index funds or ETFs",
+              "one_month_abs_return" : "5.31",
+              "one_year_XIRR_return" : "63.99",
+              "three_year_XIRR_return" : "21.75",
+              "five_year_XIRR_return" : "N/A",
+            },
+            {
+              "fund_name" : "Bandhan Small Cap Fund Regular Plan - IDCW payout",
+              "latest_nav" : "37.01",
+              "like" : "0",
+              "fund_type" : "equity-Small cap fund",
+              "one_month_abs_return" : "9.60",
+              "one_year_XIRR_return" : "61.77",
+              "three_year_XIRR_return" : "22.27",
+              "five_year_XIRR_return" : "N/A",
+            }
+          ]
+        },
+        {
+          "scheme_name" : "Vishal Sir Fund Recommended",
+          "scheme_id": "109",
+          "funds_list" : [
+            {
+              "fund_name" : "360 One Focused Equity Fund Regular Plan - Growth",
+              "latest_nav" : "21.63",
+              "like" : "0",
+              "fund_type" : "equity-index funds or ETFs",
+              "one_month_abs_return" : "5.31",
+              "one_year_XIRR_return" : "63.99",
+              "three_year_XIRR_return" : "21.75",
+              "five_year_XIRR_return" : "N/A",
+            },
+            {
+              "fund_name" : "Bandhan Small Cap Fund Regular Plan - IDCW payout",
+              "latest_nav" : "37.01",
+              "like" : "0",
+              "fund_type" : "equity-Small cap fund",
+              "one_month_abs_return" : "9.60",
+              "one_year_XIRR_return" : "61.77",
+              "three_year_XIRR_return" : "22.27",
+              "five_year_XIRR_return" : "N/A",
+            }
+          ]
+        },
+      ]
+    });
+  }
+
+  const InvestInExistingSchemes = async (req, res) => {
+    const investor_uid = req.investor.investor_uid;
+    const  clientId = req.params.clientId;
+    const investor = await InvestorSchema.findOne({investor_uid});
+    if (!investor) {
+      return res.status(404).json({ message: 'Investor not found' });
+    }
+    const client = await clientSchema.findOne(clientId);
+      
+    if (!client) {
+        return res.status(404).json({ "status" : false, message: 'Client not found' });
+    }
+
+    return res.status(201).json({
+      "status" : true,
+      "data" :  [
+        {
+          "fund_name" : "360 One Focused Equity Fund Regular Plan - Growth",
+          "latest_nav" : "21.63",
+          "like" : "0",
+          "fund_type" : "equity-index funds or ETFs",
+          "one_month_abs_return" : "5.31",
+          "one_year_XIRR_return" : "63.99",
+          "three_year_XIRR_return" : "21.75",
+          "five_year_XIRR_return" : "N/A",
+          "moh":"single",
+          "total_units":"2.37",
+          "free_units":"2.37",
+          "current_value":"4,406.04",
+       
+        },
+        {
+          "fund_name" : "Bandhan Small Cap Fund Regular Plan - IDCW payout",
+          "latest_nav" : "37.01",
+          "like" : "0",
+          "fund_type" : "equity-Small cap fund",
+          "one_month_abs_return" : "9.60",
+          "one_year_XIRR_return" : "61.77",
+          "three_year_XIRR_return" : "22.27",
+          "five_year_XIRR_return" : "N/A",
+          "moh":"single",
+          "total_units":"2.37",
+          "free_units":"2.37",
+          "current_value":"4,406.04",
+        }
+      ]
+    });
+  }
+
+  const AllSchemeList = async (req, res) => {
+    const investor_uid = req.investor.investor_uid;
+    const  clientId = req.params.clientId;
+    const investor = await InvestorSchema.findOne({investor_uid});
+    if (!investor) {
+      return res.status(404).json({ message: 'Investor not found' });
+    }
+    const client = await clientSchema.findOne(clientId);
+      
+    if (!client) {
+        return res.status(404).json({ "status" : false, message: 'Client not found' });
+    }
+
+    return res.status(201).json({
+      "status" : true,
+      "data" :  [
+        {
+          "fund_name" : "ICICI Pruduntial All Seasons Bond Fund - Annual  IdcW Payout",
+          "latest_nav" : "37.01",
+          "like" : "0",
+          "fund_type" : "equity-Small cap fund",
+          "one_month_abs_return" : "9.60",
+          "one_year_XIRR_return" : "61.77",
+          "three_year_XIRR_return" : "22.27",
+          "five_year_XIRR_return" : "N/A",
+        },
+        {
+          "fund_name" : "360 One Focused Equity Fund Regular Plan - Growth",
+          "latest_nav" : "21.63",
+          "like" : "0",
+          "fund_type" : "equity-index funds or ETFs",
+          "one_month_abs_return" : "5.31",
+          "one_year_XIRR_return" : "63.99",
+          "three_year_XIRR_return" : "21.75",
+          "five_year_XIRR_return" : "N/A",    
+        },
+        {
+          "fund_name" : "Bandhan Small Cap Fund Regular Plan - IDCW payout",
+          "latest_nav" : "37.01",
+          "like" : "0",
+          "fund_type" : "equity-Small cap fund",
+          "one_month_abs_return" : "9.60",
+          "one_year_XIRR_return" : "61.77",
+          "three_year_XIRR_return" : "22.27",
+          "five_year_XIRR_return" : "N/A",
+        }
+      ]
+    });
+  }
   
 
-  module.exports = { GetCurrentTransactionLog, GetFutureTransactionLog };
+
+  module.exports = { GetCurrentTransactionLog, GetFutureTransactionLog, UpdateTransactionExecutionDate, InvestInTopSchemes, InvestInExistingSchemes, AllSchemeList };
--- src/routes/transaction_routes.js
+++ src/routes/transaction_routes.js
@@ -1,13 +1,13 @@
-
 const express = require('express');
 const router = express.Router();
 const{ authenticateToken,  } = require('../middlewares/auth_middleware');
-const { GetCurrentTransactionLog, GetFutureTransactionLog } = require('../controllers/transact_controller')
-
-
+const { GetCurrentTransactionLog, GetFutureTransactionLog, UpdateTransactionExecutionDate, InvestInTopSchemes, InvestInExistingSchemes, AllSchemeList } = require('../controllers/transact_controller')
 
 router.post('/:clientId/get_current_transaction_log', authenticateToken, GetCurrentTransactionLog);
 router.post('/:clientId/get_future_transaction_log', authenticateToken, GetFutureTransactionLog);
-
+router.patch('/update_execution_date_future_transaction', authenticateToken, UpdateTransactionExecutionDate);
+router.get('/schemes_list', authenticateToken, InvestInTopSchemes);
+router.get('/existing_scheme', authenticateToken, InvestInExistingSchemes);
+router.get('/all_scheme_list', authenticateToken, AllSchemeList);
 
 module.exports = router;
