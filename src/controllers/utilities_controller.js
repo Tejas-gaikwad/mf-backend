dab8b1fd621915e83c8d0f096d1a49b8e1bd586f
Tejas Gaikwad <tejasgaikwad0504@gmail.com>
2024-09-11 15:41:33 +0530

    utilities api's fone

--- src/controllers/utilities_controller.js
+++ src/controllers/utilities_controller.js
@@ -78,8 +78,76 @@ const GetMergedClients = async (req, res) => {
         error: err.message,
       });
     }
-  };
+};
   
 
+  const DifferClient = async (req, res) => {
+    try {
+      const { main_client_id } = req.body;
+    
+      if (!main_client_id) {
+        return res.status(404).json({ error: 'Client ID is required' });
+      }
 
-module.exports = { MergeClients, GetMergedClients };
+      const mergedClientDocument = await MergedClients.findOne({ main_client: main_client_id });
+
+      if (!mergedClientDocument) {
+        return res.status(404).json({ error: 'No merged client document found for the given client ID' });
+      }
+
+      const { investor_id } = mergedClientDocument;
+
+      await MergedClients.findByIdAndDelete(mergedClientDocument._id);
+
+      await InvestorSchema.updateOne(
+        { _id: new mongoose.Types.ObjectId(investor_id) },
+        { $pull: { merged_clients_ids: mergedClientDocument._id } }
+      );
+  
+      return res.status(200).json({
+        status: true,
+        message: 'Merged client document deleted successfully and client removed from investor\'s merged list',
+      });
+      
+    } catch (err) {
+      return res.status(500).json({
+        status: false,
+        message: 'Error, something went wrong.',
+        error: err.message,
+      });
+    }
+  };
+
+  const GetAllMergeClients = async (req, res) => {
+    try {
+      const investor_uid = req.investor.investor_uid;
+      const investor = await InvestorSchema.findOne( {investor_uid} )
+            .populate({
+                path: 'merged_clients_ids',
+                populate: [
+                    { path: 'main_client', model: 'Client' }, // Populate main client details
+                    { path: 'merged_client_list', model: 'Client' } // Populate merged clients details
+                ]
+            });
+   
+      if (!investor) {
+        return res.status(404).json({ message: 'Investor not found' });
+      }
+      const mergedClients = investor.merged_clients_ids;
+      return res.status(200).json({
+        status: true,
+        message: 'Merged clients retrieved successfully',
+        data: mergedClients
+      });
+      
+    } catch (err) {
+      return res.status(500).json({
+        status: false,
+        message: 'Error, something went wrong.',
+        error: err.message,
+      });
+    }
+  };
+
+
+module.exports = { MergeClients, GetMergedClients, DifferClient, GetAllMergeClients};
--- src/routes/utilities_routes.js
+++ src/routes/utilities_routes.js
@@ -1,13 +1,14 @@
 
 const express = require('express');
-const {MergeClients, GetMergedClients } = require("../controllers/utilities_controller");
+const {MergeClients, GetMergedClients, DifferClient, GetAllMergeClients } = require("../controllers/utilities_controller");
 const router = express.Router();
 const{ authenticateToken,  } = require('../middlewares/auth_middleware');
 
-
-
 router.post('/merge_client', authenticateToken, MergeClients);
 router.get('/:mergedClientId/get_merge_client', authenticateToken, GetMergedClients);
+router.post('/differ_client', authenticateToken, DifferClient);
+router.get('/get_all_merge_client', authenticateToken, GetAllMergeClients);
+
 
 
 module.exports = router;
